bfb34b9ef9c02c1c7a5e8abd7d919169c74c40c5
devalexxx <46360108+devalexxx@users.noreply.github.com>
2025-01-27 17:02:02 +0100

    feat: add Linq.Select extension for EnumArray

--- Assets/Script/Utils/EnumArray.cs
+++ Assets/Script/Utils/EnumArray.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Mekaiju.Utils
@@ -56,6 +57,19 @@ namespace Mekaiju.Utils
 
         public IEnumerator<TValue> GetEnumerator() => new Enumerator(_array);
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public static class EnumArrayExtensions
+    {
+        public static EnumArray<K, V> Select<F, K, V>(this EnumArray<K, F> self, Func<F, V> selector) where K: Enum
+        {
+            EnumArray<K, V> t_ret = new();
+            foreach (var (i, v) in self.Select((v, i) => (i, v)))
+            {
+                t_ret[(K)Enum.ToObject(typeof(K), i)] = selector(v);
+            }
+            return t_ret;
+        }
     }
 
 }
