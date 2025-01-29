9850e749fb9f3ed01c52d0b013ee43a10b9b35cd
Xoletis <47059364+Xoletis@users.noreply.github.com>
2025-01-29 09:39:30 +0100

    suppression du recule

--- Assets/IA/Script/TestAI.cs
+++ Assets/IA/Script/TestAI.cs
@@ -1,9 +1,13 @@
+using MyBox;
 using UnityEngine;
 
 namespace Mekaiju.AI
 {
     public class TestAI : BasicAI
     {
+        [Separator("Combat Stat")]
+        public float test;
+
         public override void Agro()
         {
             base.Agro();
