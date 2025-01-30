65eb116069b057218cde9aea93f0479ce81e6c1c
Xoletis <47059364+Xoletis@users.noreply.github.com>
2025-01-30 09:29:13 +0100

    kaiju avencer + reculer

--- Assets/Scripts/IA/TestAI.cs
+++ Assets/Scripts/IA/TestAI.cs
@@ -1,22 +1,39 @@
 using Mekaiju.Attribute;
 using MyBox;
-using System.ComponentModel;
+using UnityEngine;
 
 namespace Mekaiju.AI
 {
     public class TestAI : BasicAI
     {
         [Foldout("Attaque de face")]
-        [OverrideLabel("Degat")]
+        [OverrideLabel("Damage")]
         public int attack1dmg = 5;
+        [PositiveValueOnly]
+        [OverrideLabel("Range")]
+        public float attack1Range = 5;
+
+        [Foldout("Debug")]
+        [OverrideLabel("Show Gizmo For Face Attack")]
+        public bool debugAttak1 = false;
+        [OverrideLabel("Color For Face Attack Range")]
+        [ConditionalField(nameof(debugAttak1))] public Color debugAttack1RangeColor = Color.red;
 
 
         public override void Agro()
         {
             base.Agro();
-
-            //_agent.destination = _target;
+            
         }
 
+        protected void OnDrawGizmos()
+        {
+            base.OnDrawGizmos();
+            if (debugAttak1)
+            {
+                Gizmos.color = debugAttack1RangeColor;
+                Gizmos.DrawWireSphere(transform.position, attack1Range);
+            }
+        }
     }
 }
