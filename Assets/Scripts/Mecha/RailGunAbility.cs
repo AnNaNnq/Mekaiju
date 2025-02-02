6c0bbaec723c9da8fcbc15f61edfd6778a531a00
devalexxx <46360108+devalexxx@users.noreply.github.com>
2025-02-02 16:02:56 +0100

    feat: add generic option to ability Execute method

--- Assets/Scripts/Mecha/RailGunAbility.cs
+++ Assets/Scripts/Mecha/RailGunAbility.cs
@@ -64,60 +64,62 @@ namespace Mekaiju
         /// <param name="p_self"></param>
         /// <param name="p_target"></param>
         /// <returns></returns>
-        public override IEnumerator Execute(MechaInstance p_self, BasicAI p_target)
+        public override IEnumerator Execute(MechaInstance p_self, BasicAI p_target, object p_opt)
         {
             var t_now     = Time.time; 
             var t_elapsed = t_now - _lastTriggerTime;
             if (t_elapsed >= _minTimeBetweenFire)
             {
-                if (p_self.CanExecuteAbility(_consumption))
-                {   
-                    _lastTriggerTime = t_now;
-                    // TODO: Launch animation
+                _lastTriggerTime = t_now;
+                // TODO: Launch animation
 
-                    p_self.ConsumeStamina(_consumption);
+                p_self.ConsumeStamina(_consumption);
 
-                    var t_targetPosition = p_target.transform.position;
+                var t_targetPosition = p_target.transform.position;
 
-                    // Compute travel time
-                    var t_dist = Vector3.Distance(p_self.transform.position, t_targetPosition);
-                    var t_time = t_dist / _travelTime;
+                // Compute travel time
+                var t_dist = Vector3.Distance(p_self.transform.position, t_targetPosition);
+                var t_time = t_dist / _travelTime;
 
-                    bool t_hasCollide = false;
+                bool t_hasCollide = false;
 
-                    var t_go = GameObject.Instantiate(_projectile);
-                    var t_wb = t_go.GetComponent<WeaponBullet>();
+                var t_go = GameObject.Instantiate(_projectile);
+                var t_wb = t_go.GetComponent<WeaponBullet>();
 
-                    t_wb.transform.position = p_self.transform.position + new Vector3(0, 2f, 2f);
-                    t_wb.OnCollide.AddListener(
-                        collision => {
-                            Debug.Log(collision.gameObject.name);
-                            if (collision.gameObject.name == "Kaiju")
-                            {
-                                t_hasCollide = true;
-                                GameObject.Destroy(t_wb);
-                            }
+                t_wb.transform.position = p_self.transform.position + new Vector3(0, 2f, 2f);
+                t_wb.OnCollide.AddListener(
+                    collision => {
+                        Debug.Log(collision.gameObject.name);
+                        if (collision.gameObject.name == "Kaiju")
+                        {
+                            t_hasCollide = true;
+                            GameObject.Destroy(t_wb);
                         }
-                    );
-                    t_wb.Launch(p_self.transform.forward.normalized * _travelTime);
-                    
-
-                    // Wait for bullet travel
-                    float t_timout = t_time;
-                    yield return new WaitUntil(() => t_hasCollide || (t_timout -= Time.deltaTime) <= 0);
-
-                    // Check new position of BasicAI ?
-                    if (t_hasCollide)
-                    {
-                        // Apply damage
-                        Debug.Log("RailGun projectile reach its target!");
                     }
-                    else
-                    {
-                        GameObject.Destroy(t_go);
-                    }
+                );
+                t_wb.Launch(p_self.transform.forward.normalized * _travelTime);
+                
+
+                // Wait for bullet travel
+                float t_timout = t_time;
+                yield return new WaitUntil(() => t_hasCollide || (t_timout -= Time.deltaTime) <= 0);
+
+                // Check new position of BasicAI ?
+                if (t_hasCollide)
+                {
+                    // Apply damage
+                    Debug.Log("RailGun projectile reach its target!");
+                }
+                else
+                {
+                    GameObject.Destroy(t_go);
                 }
             }
+        }
+
+        public override float Consumption(object p_opt)
+        {
+            return _consumption;
         }
     }
 
