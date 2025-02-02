26f340f44218c61660ee7ea3f8439e73e472acb2
devalexxx <46360108+devalexxx@users.noreply.github.com>
2025-02-02 21:13:48 +0100

    change: add Tick and FixedTick to both ability and effect behaviour to allow common and physics process (IAbilityBehaviour.Execute -> Trigger for better understanding)

--- Assets/Scripts/Mecha/IAbilityBehaviour.cs
+++ Assets/Scripts/Mecha/IAbilityBehaviour.cs
@@ -18,23 +18,42 @@ namespace Mekaiju
         }
 
         /// <summary>
-        /// 
+        /// Simple overload of <see cref="Trigger(MechaInstance,BasicAI,object)"/>
         /// </summary>
         /// <param name="p_self"></param>
         /// <param name="p_target"></param>
         /// <returns></returns>
-        public virtual IEnumerator Execute(MechaInstance p_self, BasicAI p_target)
+        public virtual IEnumerator Trigger(MechaInstance p_self, BasicAI p_target)
         {
-            return Execute(p_self, p_target, null);
+            return Trigger(p_self, p_target, null);
         }
 
         /// <summary>
-        /// 
+        /// Must be called whenever you want to use this ability.<br/>
+        /// Ensure that you have enough stamina (see <see cref="Consumption"/>)
         /// </summary>
         /// <param name="p_self"></param>
         /// <param name="p_target"></param>
         /// <returns></returns>
-        public abstract IEnumerator Execute(MechaInstance p_self, BasicAI p_target, object p_opt);
+        public abstract IEnumerator Trigger(MechaInstance p_self, BasicAI p_target, object p_opt);
+
+        /// <summary>
+        /// Must be called in <see cref="MonoBehviour.Update"/> to allow some common process.
+        /// </summary>
+        /// <param name="p_self"></param>
+        public virtual void Tick(MechaInstance p_self)
+        {
+
+        }
+
+        /// <summary>
+        /// Must be called in <see cref="MonoBehviour.FixedUpdate"/> to allow some physics process.
+        /// </summary>
+        /// <param name="p_self"></param>
+        public virtual void FixedTick(MechaInstance p_self)
+        {
+
+        }
 
         /// <summary>
         /// 
