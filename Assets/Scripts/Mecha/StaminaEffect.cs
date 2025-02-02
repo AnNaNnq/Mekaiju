1722e1fac3a5a608c26272dbdba7f86995fb4f9c
devalexxx <46360108+devalexxx@users.noreply.github.com>
2025-02-02 16:10:10 +0100

    feat: add stamina restoration by 5%/s after 2sec

---
+++ Assets/Scripts/Mecha/StaminaEffect.cs
@@ -0,0 +1,16 @@
+using System;
+using Mekaiju;
+using UnityEngine;
+
+[Serializable]
+public class StaminaEffect : IEffectBehaviour
+{
+    [SerializeField, Range(0f, 1f)]
+    private float _percentPerSec;
+
+    public override void Tick(MechaInstance self)
+    {
+        if (Time.time - self.Context.LastAbilityTime > 2f)
+            self.RestoreStamina(_percentPerSec * self.Desc.Stamina * Time.deltaTime);
+    }
+}
