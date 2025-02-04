ca0ec1405b128ec80760c9bd4cc26890e063c90e
Reyane <reyane.redjem@univ-lyon2.fr>
2025-02-04 15:45:12 +0100

    consommation de la stamina

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using Mekaiju;
 using Mekaiju.AI;
+using MyBox;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -13,28 +14,35 @@ public class PlayerController : MonoBehaviour
 
     private Animator _animator;
 
-    private MechaPlayerActions _playerActions;
+    private MechaPlayerActions _playerActions; // NewInputSystem reference
 
     private InputAction _moveAction;
     private InputAction _lookAction;
 
     private Rigidbody _rigidbody;
 
+
+    [Foldout("Movement Attributes")]
     [SerializeField] private float _jumpForce = 5f;
     [SerializeField] private float _dashForce = 20f;
     [SerializeField] private float _dashDuration = 0.25f;
     [SerializeField] private float _baseSpeed = 5f;
+    private Vector3 _dashDirection;
     private float _speed;
     //[SerializeField] private float _cameraSpeed = 5f;
 
-    [SerializeField]
-    private bool _isGrounded;
-    private bool _isDashing;
-    private bool _isProtected;
-    private Vector3 _dashDirection;
+    [Foldout("Movement Boolean")]
+    [SerializeField] bool _isGrounded;
+    [SerializeField] private bool _isDashing;
+    [SerializeField] private bool _isProtected;
+
+    [Foldout("Stamina Costs")]
+    [SerializeField] private float _shieldCost = 2f;
+    [SerializeField] private float _jumpCost = 10f;
+    [SerializeField] private float _dashCost = 33f;
 
     private MechaInstance _instance;
-
+    private Coroutine _shieldStaminaDrainCoroutine;
     private LayerMask _groundLayerMask;
 
     private void Awake()
@@ -103,53 +111,76 @@ public class PlayerController : MonoBehaviour
     {
         float t_shieldSpeedModifier = 0.5f;
 
-        _animator.SetTrigger("shield");
         _isProtected = true;
+        _animator.SetBool("IsShielding", _isProtected);
         _speed = _baseSpeed * t_shieldSpeedModifier;
+
+        // Démarre la consommation de stamina
+        if (_shieldStaminaDrainCoroutine != null) StopCoroutine(_shieldStaminaDrainCoroutine);
+        _shieldStaminaDrainCoroutine = StartCoroutine(ShieldStaminaDrain());
     }
     private void OnUnshield(InputAction.CallbackContext p_context)
     {
         _isProtected = false;
-        _animator.SetTrigger("unshield");
+        _animator.SetBool("IsShielding", _isProtected);
         _speed = _baseSpeed;
+
+        // Arrête la consommation de stamina
+        if (_shieldStaminaDrainCoroutine != null)
+        {
+            StopCoroutine(_shieldStaminaDrainCoroutine);
+            _shieldStaminaDrainCoroutine = null;
+        }
     }
+
+    private IEnumerator ShieldStaminaDrain()
+    {
+        while (_isProtected && _instance.CanExecuteAbility(_shieldCost))
+        {
+            // Consomme 2 points de stamina par seconde
+            _instance.ConsumeStamina(2f);
+            _instance.Context.LastAbilityTime = Time.time;
+
+            // Si la stamina est épuisée, désactive le bouclier
+            if (!_instance.CanExecuteAbility(_shieldCost))
+            {
+                OnUnshield(new InputAction.CallbackContext());
+                yield break;
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
     private void OnJump(InputAction.CallbackContext p_context)
     {
         if (_isGrounded && _instance.CanExecuteAbility(10f))
         {
-            _instance.ConsumeStamina(10);
+            _instance.ConsumeStamina(_jumpCost);
+            _instance.Context.LastAbilityTime = Time.time;
             _isGrounded = false;
             _animator.SetTrigger("Jump");
             _rigidbody.AddForce(Vector3.up  * _jumpForce, ForceMode.Impulse);
         }
     }
-    //private void OnHover(InputAction.CallbackContext p_context)
-    //{
-    //    Debug.Log("Hover");
-    //}
-    //private void OnStopHover(InputAction.CallbackContext p_context)
-    //{
-    //    Debug.Log("StopHover");
-    //}
 
     private void OnDash(InputAction.CallbackContext p_context)
     {
-        if (!_isDashing && !_isProtected && _instance.CanExecuteAbility(33f))
+        if (!_isDashing && !_isProtected && _instance.CanExecuteAbility(_dashCost))
         {
-            // Determine dash direction based on movement input
+            // determine direction du dash basé sur la direction de deplacement
             Vector2 moveInput = _moveAction.ReadValue<Vector2>();
             _dashDirection = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
 
-            // If no input, dash forward relative to player's facing direction
+            // Si aucune input, pas de dash
             if (_dashDirection.sqrMagnitude == 0f)
             {
-
                 //CHANGER ICI POUR CHANGER LE COMPORTEMENT QUAND LE JOUEUR DASH SANS DIRECTION
                 return;
-                //_dashDirection = transform.forward;
             }
 
-            _instance.ConsumeStamina(33);
+            _instance.ConsumeStamina(_dashCost);
+            _instance.Context.LastAbilityTime = Time.time;
             StartCoroutine(DashCoroutine());
         }
     }
@@ -157,18 +188,6 @@ public class PlayerController : MonoBehaviour
     private IEnumerator DashCoroutine()
     {
         _isDashing = true;
-        yield return new WaitForSeconds(_dashDuration);
-        _isDashing = false;
-    }
-
-
-    private IEnumerator Dash(Vector2 p_moveDir,Vector3 p_vel)
-    {
-        Debug.Log("Dash");
-        _isDashing = true;
-
-        _rigidbody.linearVelocity = new Vector3(_dashForce * p_moveDir.x,p_vel.y,_dashForce*p_moveDir.y);
-
         yield return new WaitForSeconds(_dashDuration);
         _isDashing = false;
     }
