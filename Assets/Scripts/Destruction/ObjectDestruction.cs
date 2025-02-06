3213923eea0a36d2fbeb0819218f0d7b1a437951
Xoletis <47059364+Xoletis@users.noreply.github.com>
2025-02-06 10:19:07 +0100

    destructioné

--- Assets/Scripts/Destruction/ObjectDestruction.cs
+++ Assets/Scripts/Destruction/ObjectDestruction.cs
@@ -1,17 +1,65 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Mekaiju.Destruction
 {
     public class ObjectDestruction : MonoBehaviour
     {
+        public DestructionType destructionType;
+        public GameObject destructionParticuleEffect;
+        Material _mat;
+
+        private void Start()
+        {
+            _mat = GetComponent<Renderer>().material;
+        }
+
         private void OnCollisionEnter(Collision collision)
         {
-            if(collision.collider.CompareTag("Player"))
+            if (collision.collider.CompareTag("Player") || collision.collider.CompareTag("Kaiju"))
             {
-                Rigidbody t_playerRb = collision.collider.GetComponent<Rigidbody>();
-                float t_force = Mathf.Round(t_playerRb.linearVelocity.magnitude * 100f) / 100f;
-                Debug.Log(t_force);
+                float t_force = Mathf.Max(Mathf.Abs(collision.impulse.x), Mathf.Abs(collision.impulse.y), Mathf.Abs(collision.impulse.z));
+                switch (destructionType)
+                {
+                    case DestructionType.None:
+                        break;
+                    case DestructionType.Walk: // Walk
+                        if (t_force >= 2)
+                        {
+                            StartCoroutine(FadOutAnim());
+                        }
+                        break;
+                    case DestructionType.Dash: // Dash
+                        if (t_force >= 10)
+                        {
+                            StartCoroutine(FadOutAnim());
+                        }
+                        break;
+                    default: break;
+                }
+
+                GameObject t_part = Instantiate(destructionParticuleEffect, transform.position, Quaternion.identity);
+                Destroy(t_part, 2);
             }
+        }
+
+        IEnumerator FadOutAnim()
+        {
+            while (_mat.color.a > 0)
+            {
+                float alpha = _mat.color.a;
+                _mat.color = new Color(_mat.color.r, _mat.color.g, _mat.color.b, alpha - 0.01f);
+                yield return new WaitForSeconds(0.01f);
+            }
+            GetComponent<Renderer>().enabled = false;
+            GetComponent<Collider>().enabled = false;
+        }
+
+        public enum DestructionType
+        {
+            None,
+            Walk,
+            Dash
         }
     }
 }
