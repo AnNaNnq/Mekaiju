abdf8875949458a857b72261a65e31f7b6c9d6cb
devalexxx <46360108+devalexxx@users.noreply.github.com>
2025-02-09 15:42:26 +0100

    fix: move jump ability to correct namespace

--- Assets/Scripts/Mecha/JumpAbility.cs
+++ Assets/Scripts/Mecha/JumpAbility.cs
@@ -1,57 +1,59 @@
 using System.Collections;
-using Mekaiju;
 using Mekaiju.AI;
 using UnityEngine;
 
-/// <summary>
-/// 
-/// </summary>
-public class JumpAbility : IAbilityBehaviour
+namespace Mekaiju
 {
     /// <summary>
     /// 
     /// </summary>
-    [SerializeField]
-    private float _force;
-
-    /// <summary>
-    /// 
-    /// </summary>
-    [SerializeField]
-    private int _consumption;
-
-    /// <summary>
-    /// 
-    /// </summary>
-    private bool _requested;
-
-    public override void Initialize()
+    public class JumpAbility : IAbilityBehaviour
     {
-        _requested = false;
-    }
+        /// <summary>
+        /// 
+        /// </summary>
+        [SerializeField]
+        private float _force;
 
-    public override IEnumerator Trigger(MechaPartInstance p_self, BodyPartObject p_target, object p_opt)
-    {  
-        if (p_self.Mecha.Context.IsGrounded && !_requested)
-        {
-            p_self.Mecha.ConsumeStamina(_consumption);
-            _requested = true;
-        }
-        yield return null;
-    }
+        /// <summary>
+        /// 
+        /// </summary>
+        [SerializeField]
+        private int _consumption;
 
-    public override void FixedTick(MechaPartInstance p_self)
-    {
-        if (_requested)
+        /// <summary>
+        /// 
+        /// </summary>
+        private bool _requested;
+
+        public override void Initialize()
         {
-            p_self.Mecha.Context.Animator.SetTrigger("Jump");
-            p_self.Mecha.Context.Rigidbody.AddForce(Vector3.up * _force, ForceMode.Impulse);
             _requested = false;
         }
-    }
 
-    public override float Consumption(object p_opt)
-    {
-        return _consumption;
+        public override IEnumerator Trigger(MechaPartInstance p_self, BodyPartObject p_target, object p_opt)
+        {  
+            if (p_self.Mecha.Context.IsGrounded && !_requested)
+            {
+                p_self.Mecha.ConsumeStamina(_consumption);
+                _requested = true;
+            }
+            yield return null;
+        }
+
+        public override void FixedTick(MechaPartInstance p_self)
+        {
+            if (_requested)
+            {
+                p_self.Mecha.Context.Animator.SetTrigger("Jump");
+                p_self.Mecha.Context.Rigidbody.AddForce(Vector3.up * _force, ForceMode.Impulse);
+                _requested = false;
+            }
+        }
+
+        public override float Consumption(object p_opt)
+        {
+            return _consumption;
+        }
     }
 }
