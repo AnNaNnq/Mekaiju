11158887badea1d9503bd4c64d9c9483694c79b2
Xoletis <47059364+Xoletis@users.noreply.github.com>
2025-02-12 16:31:08 +0100

    Fissure du néant dégat + animation de fin

--- Assets/Scripts/KaijuAttacks/RimVoidFire.cs
+++ Assets/Scripts/KaijuAttacks/RimVoidFire.cs
@@ -1,3 +1,5 @@
+using Mekaiju;
+using Mekaiju.AI;
 using System.Collections;
 using UnityEngine;
 
@@ -5,8 +7,13 @@ public class RimVoidFire : MonoBehaviour
 {
     public int maxSizeY = 10;
 
-    public void UpdateLineVisual(LineRenderer p_line)
+    TeneborokAI _ai;
+
+    bool _damagable = false;
+
+    public void UpdateLineVisual(LineRenderer p_line, TeneborokAI p_ai)
     {
+        _ai = p_ai;
         int pointCount = p_line.positionCount;
         if (pointCount < 2) return; // Il faut au moins deux points pour tracer une ligne
 
@@ -26,6 +33,7 @@ public class RimVoidFire : MonoBehaviour
         float length = Vector3.Distance(startPos, endPos);
         gameObject.transform.localScale = new Vector3(0.2f, 0.1f, length); // 0.2f = épaisseur
         StartCoroutine(GrowthWall());
+        StartCoroutine(DestroyWall());
     }
 
     IEnumerator GrowthWall()
@@ -35,6 +43,46 @@ public class RimVoidFire : MonoBehaviour
             yield return new WaitForSeconds(0.01f);
             Vector3 t_scale = gameObject.transform.localScale;
             gameObject.transform.localScale = new Vector3(t_scale.x, t_scale.y + 0.5f, t_scale.z);
+        }
+    }
+
+    IEnumerator DestroyWall()
+    {
+        yield return new WaitForSeconds(_ai.rimDuration - 1);
+        while (gameObject.transform.localScale.y >= 0)
+        {
+            yield return new WaitForSeconds(0.01f);
+            Vector3 t_scale = gameObject.transform.localScale;
+            gameObject.transform.localScale = new Vector3(t_scale.x, t_scale.y - 0.5f, t_scale.z);
+        }
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MechaInstance t_mecha = other.GetComponent<MechaInstance>();
+            _damagable = true;
+            StartCoroutine(Damage(t_mecha));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _damagable = false;
+        }
+    }
+
+    IEnumerator Damage(MechaInstance p_mecha)
+    {
+        while (_damagable)
+        {
+            p_mecha.TakeDamage(_ai.rimDamage);
+            _ai.AddDps(_ai.rimDamage);
+            yield return new WaitForSeconds(0.1f);
         }
     }
 }
