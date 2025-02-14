e802b5b04b461a9c4e285ad758870496723e2d70
AnNaNnq <97672945+AnNaNnq@users.noreply.github.com>
2025-02-14 17:08:02 +0100

    feat: vfx rock falling + impact

--- Assets/Scripts/Kaijus/KaijuAttacks/FallRock.cs
+++ Assets/Scripts/Kaijus/KaijuAttacks/FallRock.cs
@@ -1,11 +1,14 @@
 using Mekaiju;
 using Mekaiju.AI;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class FallRock : MonoBehaviour
 {
     public int _dmg;
     TeneborokAI _ia;
+    public GameObject impactVFX;
+
 
     public void SetUp(TeneborokAI p_ai)
     {
@@ -15,11 +18,16 @@ public class FallRock : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.tag);
         if (other.gameObject.CompareTag("Player"))
         {
             other.gameObject.GetComponent<MechaInstance>().TakeDamage(_dmg);
             _ia.AddDps(_dmg);
+        }
+        else
+        {
+            GameObject t_impactVFX = Instantiate(impactVFX, transform.position, Quaternion.identity);
+            Destroy(t_impactVFX, 2);
+            Destroy(gameObject);
         }
     }
 }
