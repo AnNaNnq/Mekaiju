ef44b67197f0aafb39b003f869c09d785221916a
Xoletis <47059364+Xoletis@users.noreply.github.com>
2025-02-20 16:47:07 +0100

    rework: kaiju attack base

--- Assets/Scripts/Kaijus/KaijuAttacks/IAttack.cs
+++ Assets/Scripts/Kaijus/KaijuAttacks/IAttack.cs
@@ -4,6 +4,19 @@ namespace Mekaiju.AI {
     [System.Serializable]
     public abstract class IAttack
     {
+        public float cooldown;
+        public float range;
+        protected bool canUse { get; set; }
+
+        public bool CanUse(KaijuInstance kaiju, float otherRange = 0)
+        {
+            bool t_return = canUse && kaiju.TargetInRange(range);
+            if (otherRange > 0)
+            {
+                t_return &= !kaiju.TargetInRange(otherRange);
+            }
+            return t_return;
+        }
 
         public virtual void Active() { }
     }
--- Assets/Scripts/Kaijus/KaijuBrain.cs
+++ Assets/Scripts/Kaijus/KaijuBrain.cs
@@ -12,6 +12,7 @@ namespace Mekaiju.AI
         private KaijuAttackContainer _attackGraph;
         private string _lastAttack;
 
+        [SerializeField]
         private KaijuAttack[] _allAttacks;
 
         private void Start()
@@ -23,12 +24,16 @@ namespace Mekaiju.AI
 
         public void StarFight()
         {
-            string t_GUID = _attackGraph.StartNodeData
-            .FirstOrDefault(attack => attack.Name == "Start")?.GUID;
-
-            //The first attack necessarily has only one link (not 2 not 0)
-            string t_startAttack = GetNextNodes(t_GUID)[0];
+            string t_GUID = GetGUIDStartWithName("Start");
+            
+            List<string> t_startAttack = GetNextNodes("b5a74a81-edc1-4018-95f2-606b6fc917c3");
             Attack(t_startAttack);
+        }
+
+        public string GetGUIDStartWithName(string p_name)
+        {
+            return _attackGraph.StartNodeData
+            .FirstOrDefault(attack => attack.name == p_name)?.guid;
         }
 
         public List<string> GetNextNodes(string p_NodeGUID)
@@ -41,28 +46,44 @@ namespace Mekaiju.AI
                 {
                     string t_target = link.TargetNodeGUID;
 
-                    var t_match = _attackGraph.NodeData.FirstOrDefault(item => item.GUID == t_target);
+                    var t_match = _attackGraph.NodeData.FirstOrDefault(item => item.guid == t_target);
 
                     if (t_match != null)
                     {
-                        t_nodesGUID.Add(t_match.GUID);
+                        t_nodesGUID.Add(t_match.guid);
                     }
                 }
             }
             return t_nodesGUID;
         }
 
+        public List<KaijuAttack> PotentialAttacks(List<string> p_attacksGUID)
+        {
+            List<KaijuAttack> t_kaijuAttacks = new List<KaijuAttack>();
+            foreach(string attack in p_attacksGUID)
+            {
+                t_kaijuAttacks.Add(GetAttack(GetNameWithGUID(attack)));
+            }
+
+            return t_kaijuAttacks;
+        }
+
+        public string GetNameWithGUID(string p_guid)
+        {
+            return _attackGraph.NodeData.FirstOrDefault(attack => attack.guid == p_guid)?.name;
+        }
+
         public KaijuAttack[] LoadAllAttacks()
         {
             // Charger tous les ScriptableObjects de type Attack dans le dossier Resources/Kaijus/Attacks
-            KaijuAttack[] attacks = Resources.LoadAll<KaijuAttack>("Kaijus/Attacks");
+            KaijuAttack[] t_attacks = Resources.LoadAll<KaijuAttack>("Kaijus/Attacks");
 
-            if (attacks.Length == 0)
+            if (t_attacks.Length == 0)
             {
                 Debug.LogWarning("Aucun attack ScriptableObject trouvé dans Resources/Kaijus/Attacks.");
             }
 
-            return attacks;
+            return t_attacks;
         }
 
         KaijuAttack GetAttack(string p_attackName)
@@ -70,11 +91,27 @@ namespace Mekaiju.AI
             return _allAttacks.FirstOrDefault(attack => attack.Name == p_attackName);
         }
 
-        public void Attack(string p_attackName)
+        public void Attack(List<string> p_attackGUID)
         {
-            KaijuAttack t_attack = GetAttack(p_attackName);
-            t_attack.Attack.Active();
-            _lastAttack = p_attackName;
+            List<KaijuAttack> t_kaijuAttacks = PotentialAttacks(p_attackGUID);
+            Debug.Log(t_kaijuAttacks.Count());
+            for (int i = 0; i < t_kaijuAttacks.Count; i++)
+            {
+                Debug.Log(i);
+                if (i < t_kaijuAttacks.Count - 1)
+                {
+                    Debug.Log("First");
+                }
+                else
+                {
+                    Debug.Log("Last");
+                }
+            }
+
+            //string t_name = _attackGraph.NodeData.FirstOrDefault(attack => attack.GUID == p_attackGUID)?.Name;
+
+            //KaijuAttack t_attack = GetAttack(t_name);
+            //Debug.Log(t_attack.Attack.CanUse(_instance).ToString());
         }
 
     }
