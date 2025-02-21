3b99776da7088aca2a71775a29c43a8128d0d083
devalexxx <46360108+devalexxx@users.noreply.github.com>
2025-02-21 11:01:16 +0100

    feat: add animation trigger

--- Assets/Scripts/Mecha/MechaAnimatorProxy.cs
+++ Assets/Scripts/Mecha/MechaAnimatorProxy.cs
@@ -3,28 +3,76 @@ using UnityEngine.Events;
 
 namespace Mekaiju
 {
+    public enum AnimationEventType
+    {
+        Start, End, Action
+    }
+
     public class MechaAnimatorProxy : MonoBehaviour
     {
         public Animator animator;
 
-        public UnityEvent onJump;
-        public UnityEvent onFire;
+        public UnityEvent<AnimationEventType> onJump;
+        public UnityEvent<AnimationEventType> onLArm;
+        public UnityEvent<AnimationEventType> onRArm;
 
         void Awake()
         {
             animator = GetComponent<Animator>();
             onJump   = new();
-            onFire   = new();
+            onLArm   = new();
+            onRArm   = new();
         }
 
-        private void _OnJump()
+#region Jump
+        private void _OnJumpStart()
         {
-            onJump.Invoke();
+            onJump.Invoke(AnimationEventType.Start);
         }
 
-        private void _OnFire()
+        private void _OnJumpAction()
         {
-            onFire.Invoke();
+            onJump.Invoke(AnimationEventType.Action);
         }
+
+        private void _OnJumpEnd()
+        {
+            onJump.Invoke(AnimationEventType.End);
+        }
+#endregion
+
+#region LArm
+        private void _OnLArmStart()
+        {
+            onLArm.Invoke(AnimationEventType.Start);
+        }
+
+        private void _OnLArmAction()
+        {
+            onLArm.Invoke(AnimationEventType.Action);
+        }
+
+        private void _OnLArmEnd()
+        {
+            onLArm.Invoke(AnimationEventType.End);
+        }
+#endregion
+
+#region RArm
+        private void _OnRArmStart()
+        {
+            onRArm.Invoke(AnimationEventType.Start);
+        }
+
+        private void _OnRArmAction()
+        {
+            onRArm.Invoke(AnimationEventType.Action);
+        }
+
+        private void _OnRArmEnd()
+        {
+            onRArm.Invoke(AnimationEventType.End);
+        }
+#endregion
     }
 }
