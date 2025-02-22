bc8ce2df3bee5972783785fde2bf945361196060
devalexxx <46360108+devalexxx@users.noreply.github.com>
2025-02-22 17:08:15 +0100

    feat: generic entity instance

---
+++ Assets/Scripts/Entity/TimePoint.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Mekaiju
+{
+    /// <summary>
+    /// Entity time point
+    /// </summary>
+    public enum TimePoint
+    {
+        LastAbilityTriggered, LastDamage
+    }
+}
