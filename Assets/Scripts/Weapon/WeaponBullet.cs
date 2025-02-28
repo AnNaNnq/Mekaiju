352e8f71f09a790be8f4af5e2274a0df4fc1dc0b
Reyane <reyane.redjem@univ-lyon2.fr>
2025-02-28 10:46:53 +0100

    ajout noms dans credit et fix bug gravité projectiles

--- Assets/Scripts/Weapon/WeaponBullet.cs
+++ Assets/Scripts/Weapon/WeaponBullet.cs
@@ -7,10 +7,12 @@ public class WeaponBullet : MonoBehaviour
 {
     public UnityEvent<Collision> OnCollide;
     private Rigidbody _rb;
+    private Vector3 _velocity;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        _velocity = Vector3.zero;
     }
 
     private void OnCollisionEnter(Collision p_collision)
@@ -20,8 +22,13 @@ public class WeaponBullet : MonoBehaviour
 
     public void Launch(Vector3 p_velocity, Vector3 p_direction)
     {
-        _rb.linearVelocity = p_velocity;
+        _velocity = p_velocity;
         transform.rotation = Quaternion.LookRotation(p_direction);
         transform.Rotate(new (0, 90, 0));
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.linearVelocity = _velocity;
     }
 }
