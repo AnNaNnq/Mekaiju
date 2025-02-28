e156669025bc83cb37783ccf72530b34cd78f707
AnNaNnq <97672945+AnNaNnq@users.noreply.github.com>
2025-02-28 10:23:07 +0100

    fix: remove UnityEditor.ShaderGraph

--- Assets/Scripts/Kaijus/KaijuAttacks/Spawnable/RimVoidInstance.cs
+++ Assets/Scripts/Kaijus/KaijuAttacks/Spawnable/RimVoidInstance.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 namespace Mekaiju.AI.Attack
 {
