aa2813cd58a81ba1611ad3c4bd7112fbe2014779
devalexxx <46360108+devalexxx@users.noreply.github.com>
2025-03-01 12:22:51 +0100

    feat: projectile will be destroyed after any contact or a timout

--- Assets/Scripts/Mecha/Abilities/RailGunAbility.cs
+++ Assets/Scripts/Mecha/Abilities/RailGunAbility.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using Mekaiju.AI;
+using MyBox;
 using UnityEngine;
 
 namespace Mekaiju
@@ -10,40 +11,39 @@ namespace Mekaiju
     /// </summary>
     public class RailGunAbility : IAbilityBehaviour
     {
+#region Parameters
         /// <summary>
-        /// 
+        /// The damage factor (multiply the mecha damage stat)
         /// </summary>
-        [SerializeField]
-        private float _damage;
+        [Tooltip("The damage factor (multiply the mecha damage stat).")]
+        [SerializeField, Range(0f, 5f)]
+        private float _damageFactor;
 
         /// <summary>
-        /// Max bullet throw per minute
+        /// The projectil speed in m/s
         /// </summary>
-        [SerializeField]
-        private int _rateOfFire;
-
-        /// <summary>
-        /// Projectile speed in m/s
-        /// </summary>
-        [SerializeField]
+        [Tooltip("The projectil speed in m/s")]
+        [SerializeField, PositiveValueOnly]
         private float _projectileSpeed;
 
         /// <summary>
-        /// Projectile prefab (must contain WeaponBullet comp)
+        /// The stamina consumption
         /// </summary>
-        [SerializeField]
-        private GameObject _projectile;
-
-        /// <summary>
-        /// Stamina consumption for a shot
-        /// </summary>
-        [SerializeField]
+        [Tooltip("The stamina consumption")]
+        [SerializeField, PositiveValueOnly]
         private int _consumption;
 
-        private float _lastTriggerTime;
-        private float _minTimeBetweenFire => 1f / (_rateOfFire / 60f);
+        /// <summary>
+        /// The projectil prefab to be thrown (must contain WeaponBullet comp)
+        /// </summary>
+        [Tooltip("The projectil prefab to be thrown")]
+        [SerializeField]
+        private GameObject _projectile;
+#endregion
+
         private float _endTriggerTimout    = 5f;
         private float _actionTriggerTimout = 5f;
+        private float _projectileDestructionTimout = 10f;
 
         private bool _isActive;
         private bool _isAnimationAction;
@@ -51,7 +51,6 @@ namespace Mekaiju
 
         public override void Initialize(MechaPartInstance p_self)
         {
-            _lastTriggerTime = 0f;
             _isActive = false;
             _isAnimationAction = false;
             _isAnimationEnd    = false;
@@ -60,17 +59,14 @@ namespace Mekaiju
 
         public override bool IsAvailable(MechaPartInstance p_self, object p_opt)
         {
-            return !_isActive && Time.time - _lastTriggerTime >= _minTimeBetweenFire && p_self.mecha.stamina - _consumption >= 0f;
+            return !_isActive && p_self.mecha.stamina - _consumption >= 0f;
         }
 
         public override IEnumerator Trigger(MechaPartInstance p_self, BodyPartObject p_target, object p_opt)
         {
-            var t_now     = Time.time;
-            var t_elapsed = t_now - _lastTriggerTime;
-            if (t_elapsed >= _minTimeBetweenFire)
+            if (IsAvailable(p_self, p_opt))
             {
                 _isActive = true;
-                _lastTriggerTime = t_now;
 
                 p_self.mecha.context.animationProxy.animator.SetTrigger("RArm");
 
@@ -81,47 +77,30 @@ namespace Mekaiju
 
                 p_self.mecha.ConsumeStamina(_consumption);
 
-                // Compute travel time
-                var t_dist = Vector3.Distance(p_self.transform.position, p_target.transform.position);
-                var t_time = t_dist / _projectileSpeed;
-
-                bool t_hasCollide = false;
-
                 // Setup projectile and launch
-                var t_go = GameObject.Instantiate(_projectile);
-                var t_wb = t_go.GetComponent<WeaponBullet>();
-                t_wb.transform.position = p_self.transform.position + new Vector3(0, 2.5f, 0) + (2 * p_self.mecha.transform.forward);
+                var t_wb = GameObject.Instantiate(_projectile).GetComponent<WeaponBullet>();
+                t_wb.transform.position = p_self.transform.position + new Vector3(0, 2.5f, 0) + (10f * p_self.mecha.transform.forward);
                 t_wb.OnCollide.AddListener(
-                    collision => {
-                        if (collision.gameObject.TryGetComponent<BodyPartObject>(out var t_bpo))
+                    (t_go, t_collision) => 
+                    {
+                        if (t_collision.gameObject.TryGetComponent<BodyPartObject>(out var t_bpo))
                         {
-                            t_hasCollide = true;
+                            var t_damage = _damageFactor * p_self.mecha.modifiers[ModifierTarget.Damage].ComputeValue(p_self.mecha.desc.damage);
+                            p_target.TakeDamage(t_damage);
+                            p_self.onDealDamage.Invoke(t_damage);
                         }
+                        GameObject.Destroy(t_go);
                     }
                 );
                 t_wb.Launch(p_self.transform.forward.normalized * _projectileSpeed, p_self.mecha.transform.forward);
-                
-
-                // Wait for bullet travel
-                t_timout = t_time;
-                yield return new WaitUntil(() => t_hasCollide || (t_timout -= Time.deltaTime) <= 0);
-
-                // Make damage if projectile has collide
-                if (t_hasCollide)
-                {
-                    var t_damage = p_self.mecha.context.modifiers[ModifierTarget.Damage].ComputeValue(_damage);
-                    // @TODO: wait for parameter to be float
-                    p_target.TakeDamage((int)t_damage);
-                    p_self.onDealDamage.Invoke(t_damage);
-                }
-                GameObject.Destroy(t_go);
+                t_wb.Timout(_projectileDestructionTimout);
 
                 // Wait for animation end
                 t_timout = _endTriggerTimout;
                 yield return new WaitUntil(() => _isAnimationEnd || (t_timout -= Time.deltaTime) <= 0);
-
                 _isAnimationEnd = false;
-                _isActive       = false;
+
+                _isActive = false;
             }
         }
 
