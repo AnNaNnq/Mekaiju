d7ef50c490eb14418b39803cd64eb2e857c38fcd
Xoletis <47059364+Xoletis@users.noreply.github.com>
2025-03-05 09:56:17 +0100

    feat: add damage to fire zone

--- Assets/Scripts/Kaijus/Attacks/Spawnable/FireZone.cs
+++ Assets/Scripts/Kaijus/Attacks/Spawnable/FireZone.cs
@@ -1,16 +1,61 @@
+﻿using Mekaiju.AI.Attack.Instance;
+using System;
+using System.Collections;
 using UnityEngine;
 
-public class FireZone : MonoBehaviour
+namespace Mekaiju.AI.Attack
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class FireZone : MonoBehaviour
     {
-        
-    }
+        private DoomsdayRayUpgrade _stats;
+        private KaijuInstance _kaiju;
+        private bool _playerInside = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void Init(DoomsdayRayUpgrade p_stats, KaijuInstance p_kaiju)
+        {
+            _stats = p_stats;
+            _kaiju = p_kaiju;
+            _playerInside = false;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!_playerInside && other.CompareTag("Player"))
+            {
+                _playerInside = true;
+                MechaInstance t_mecha = other.GetComponent<MechaInstance>();
+                StartCoroutine(DealDamage(t_mecha));
+            }
+
+            //Pour �viter que les zones de feu ne se superposent
+            if (other.CompareTag("Spawnable"))
+            {
+                if (GetInstanceID() > other.GetInstanceID())
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        private IEnumerator DealDamage(MechaInstance p_mech)
+        {
+            while (_playerInside)
+            {
+                yield return new WaitForSeconds(_stats.fireTickRate);
+                float t_dmg = _kaiju.GetRealDamage(_stats.fireDamage);
+                p_mech.TakeDamage(t_dmg);
+
+                //Debug
+                _kaiju.AddDPS(t_dmg);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (_playerInside && other.CompareTag("Player"))
+            {
+                _playerInside = false;
+            }
+        }
     }
 }
