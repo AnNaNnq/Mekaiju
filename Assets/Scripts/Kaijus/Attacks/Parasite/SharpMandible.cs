4cc04d240d0e37e870b8b23bdcca0f2f422427bb
Sarbiewski Anna <97672945+AnNaNnq@users.noreply.github.com>
2025-03-18 15:05:25 +0100

    refactor: delete useless variable

--- Assets/Scripts/Kaijus/Attacks/Parasite/SharpMandible.cs
+++ Assets/Scripts/Kaijus/Attacks/Parasite/SharpMandible.cs
@@ -5,9 +5,6 @@ namespace Mekaiju.AI.Attack
 {
     public class SharpMandible : Attack
     {
-        [Separator]
-        public float timeBeforeAttack = 1;
-
         public override void Active(EntityInstance p_kaiju)
         {
             base.Active(p_kaiju);
