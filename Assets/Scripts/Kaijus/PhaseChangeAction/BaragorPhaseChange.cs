659f6004be3e138523fc5109c16bb35ce4e85463
Xoletis <47059364+Xoletis@users.noreply.github.com>
2025-03-20 17:46:26 +0100

    feat: barragor jump 5 time

--- Assets/Scripts/Kaijus/PhaseChangeAction/BaragorPhaseChange.cs
+++ Assets/Scripts/Kaijus/PhaseChangeAction/BaragorPhaseChange.cs
@@ -3,6 +3,7 @@ using Mekaiju.Utils;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Mekaiju.AI.PhaseAttack
@@ -15,17 +16,24 @@ namespace Mekaiju.AI.PhaseAttack
         public float chargeDuration = 1.0f;
         public float chargeSpeed = 10000;
         public float verticalBoost = 1000f;
+        public int nbJump = 5;
+
+        int _currentJump = 0;
 
         public override void Action()
         {
             base.Action();
 
             _rb = _kaiju.GetComponent<Rigidbody>();
+
+            _kaiju.brain.isStopped = true;
+
             _target = _kaiju.target.transform;
-            _kaiju.StartCoroutine(Charge());
+            _currentJump = 0;
+            _kaiju.StartCoroutine(RecursiceJump());
         }
 
-        IEnumerator Charge()
+        IEnumerator RecursiceJump()
         {
             Vector3 t_targetPosition = _kaiju.GetTargetPos();
             Vector3 t_startPos = _kaiju.transform.position;
@@ -40,6 +48,19 @@ namespace Mekaiju.AI.PhaseAttack
                 _rb.AddForce(t_direction * (chargeSpeed / chargeDuration), ForceMode.Acceleration);
                 t_elapsed += Time.deltaTime;
                 yield return null;
+            }
+
+            _currentJump++;
+
+            yield return new WaitForSeconds(1);
+
+            if (_currentJump < nbJump)
+            {
+                _kaiju.StartCoroutine(RecursiceJump());
+            }
+            else
+            {
+                _kaiju.brain.isStopped = false;
             }
         }
 
