9a2e9dd989cbfa425a980425573c0e88362f9bcb
Xoletis <47059364+Xoletis@users.noreply.github.com>
2025-03-22 10:22:55 +0100

    feat: qte and charge after change phase

--- Assets/Scripts/Kaijus/PhaseChangeAction/BaragorPhaseChange.cs
+++ Assets/Scripts/Kaijus/PhaseChangeAction/BaragorPhaseChange.cs
@@ -18,6 +18,8 @@ namespace Mekaiju.AI.PhaseAttack
         public float verticalBoost = 1000f;
         public int nbJump = 5;
 
+        public float healAmountWhenFail = 50;
+
         int _currentJump = 0;
 
         [Separator("ShockWave")]
@@ -30,6 +32,8 @@ namespace Mekaiju.AI.PhaseAttack
             _rb = _kaiju.GetComponent<Rigidbody>();
 
             _kaiju.brain.isStopped = true;
+
+            wave.kaiju = _kaiju;
 
             _target = _kaiju.target.transform;
             _currentJump = 0;
@@ -55,10 +59,10 @@ namespace Mekaiju.AI.PhaseAttack
 
             _currentJump++;
 
+            yield return new WaitForSeconds(1);
+
             IShockWave t_shockWave = this;
             t_shockWave.LunchWave(wave, _kaiju.transform);
-
-            yield return new WaitForSeconds(1);
 
             if (_currentJump < nbJump)
             {
@@ -66,8 +70,50 @@ namespace Mekaiju.AI.PhaseAttack
             }
             else
             {
-                _kaiju.brain.isStopped = false;
+                LunchQTE();
             }
+        }
+
+        void LunchQTE()
+        {
+            input = UnityEngine.Random.Range(0, _qte.qteInputActions.Count);
+
+            _qte.StartQTE();
+        }
+
+        public override void Success()
+        {
+            base.Success();
+            MechaInstance t_mecha = _kaiju.target.GetComponent<MechaInstance>();
+            _kaiju.brain.isStopped = false;
+            _kaiju.motor.StartKaiju();
+        }
+
+        public override void Failure()
+        {
+            base.Failure();
+
+            _kaiju.StartCoroutine(charge());
+
+            _kaiju.Heal(healAmountWhenFail);
+            _kaiju.SetPhase(1);
+        }
+
+        IEnumerator charge()
+        {
+            Vector3 t_targetPosition = _kaiju.GetTargetPos();
+            Vector3 t_startPos = _kaiju.transform.position;
+            Vector3 t_direction = (t_targetPosition - t_startPos).normalized;
+
+            float t_elapsed = 0f;
+            while (t_elapsed < chargeDuration)
+            {
+                _rb.AddForce(t_direction * (chargeSpeed / chargeDuration), ForceMode.Acceleration);
+                t_elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            _kaiju.brain.isStopped = false;
         }
 
     }
