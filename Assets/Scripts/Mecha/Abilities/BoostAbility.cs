a236c1b9ca8e0d2406763e0a19715beede5ad3e2
devalexxx <46360108+devalexxx@users.noreply.github.com>
2025-03-24 17:15:01 +0100

    feat: add boost ability damaged debuff

--- Assets/Scripts/Mecha/Abilities/BoostAbility.cs
+++ Assets/Scripts/Mecha/Abilities/BoostAbility.cs
@@ -5,9 +5,17 @@ using Mekaiju.Utils;
 using UnityEngine;
 using Mekaiju.Entity.Effect;
 using Mekaiju.Entity;
+using System;
 
 namespace Mekaiju
 {
+
+    [Serializable]
+    public class BoostPayload : IPayload
+    {
+        public EnumArray<StatisticKind, float> values;
+    }
+
     /// <summary>
     /// 
     /// </summary>
@@ -18,7 +26,7 @@ namespace Mekaiju
         /// </summary>
         [Header("Effect")]
         [SerializeField]
-        private Effect _boostEffect;
+        private EnumArray<StatisticKind, Modifier> _modifiers;
 
         /// <summary>
         /// 
@@ -26,20 +34,66 @@ namespace Mekaiju
         [SerializeField]
         private float _duration;
 
+        private EnumArray<StatisticKind, Modifier> _runtimeModifiers;
+
+        public override void Initialize(EntityInstance p_self)
+        {
+            base.Initialize(p_self);
+
+            _runtimeModifiers = _modifiers;
+        }
+
         public override IEnumerator Trigger(EntityInstance p_self, BodyPartObject p_target, object p_opt)
         {
             if (IsAvailable(p_self, p_opt))
             {
                 state = AbilityState.Active;
 
+                Debug.Log("ddd");
+
                 ConsumeStamina(p_self);
 
-                p_self.AddEffect(_boostEffect, _duration);
+                var t_refs = _runtimeModifiers.Select((t_key, t_value) => {
+                    return p_self.modifiers[t_key].Add(t_value.value / 100f, t_value.kind);
+                });
+
                 yield return new WaitForSeconds(_duration);
+
+                t_refs.ForEach((t_key, t_ref) => {
+                    p_self.modifiers[t_key].Remove(t_ref);
+                });
 
                 yield return WaitForCooldown();
 
                 state = AbilityState.Ready;
+            }
+        }
+
+        public override IAlteration Alter<T>(T p_payload)
+        {
+            if (p_payload is BoostPayload t_casted)
+            {
+                BoostPayload t_diff = new();
+                t_diff.values = new();
+
+                _runtimeModifiers = _modifiers.Select((t_key, t_mod) => 
+                {
+                    return new Modifier(_runtimeModifiers[t_key].value + (t_diff.values[t_key] = t_mod.value * t_casted.values[t_key]), t_mod.kind);
+                });
+
+                return new Alteration<BoostPayload>(t_casted, t_diff);
+            }
+            return null;
+        }
+
+        public override void Revert(IAlteration p_payload)
+        {
+            if (p_payload is Alteration<BoostPayload> t_casted)
+            {
+                _runtimeModifiers = _modifiers.Select((t_key, t_mod) => 
+                {
+                    return new Modifier(_runtimeModifiers[t_key].value - t_casted.diff.values[t_key], t_mod.kind);
+                });
             }
         }
     }
